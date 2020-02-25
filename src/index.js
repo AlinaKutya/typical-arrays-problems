a5d2338743a8ddec7ade62e80dc8557dc78f77ff
Alina Kutyeva <flytta.dig@mail.ru>
2020-02-25 03:23:22 +0300

    feat: add arrays functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+exports.min = function min(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let min = array[0];
 
-exports.max = function max (array) {
-  return 0;
-}
+    for (let i in array) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return min;
+};
+
+exports.max = function max(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+
+    let max = array[0];
+
+    for (let i in array) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    return max;
+};
+
+exports.avg = function avg(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+
+    let sum = array.reduce((total, number) => total + number, 0);
+
+    return sum / array.length;
+};
